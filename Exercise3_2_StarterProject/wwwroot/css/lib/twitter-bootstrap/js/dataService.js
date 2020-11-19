b999e8f25103591a5b36952a9e478e622f446738
Mathias <mip@ruc.dk>
2020-11-19 12:51:56 +0100

    jf

---
+++ Exercise3_2_StarterProject/wwwroot/css/lib/twitter-bootstrap/js/dataService.js
@@ -0,0 +1,35 @@
+﻿console.log("Before fetch");
+
+let getCategories = function (callback) {
+    fetch("api/categories")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            callback(data);
+        });
+};
+
+let createCategory = function (category, callback) {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    fetch("api/categories", { method: "POST", body: JSON.stringify(category), headers })
+        .then(response => response.json())
+        .then(data => callback(data));
+}
+
+let deleteCategory = url => fetch(url, { method: "DELETE" });
+
+
+//getCategories(function(data) {
+//    console.log(data);
+//});
+
+//createCategory({ name: 'dasdlkad', description: 'ksfjsalkdjakdl' },
+//    function(data) {
+//        console.log(data);
+//    });
+
+deleteCategory("api/categories/9");
+
+console.log("After fetch");
