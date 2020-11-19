8f2a9843dfcb8fb849536ed3a1767a9467bd42c0
Mathias <mip@ruc.dk>
2020-11-19 13:10:22 +0100

    batman

---
+++ Exercise3_2_StarterProject/wwwroot/css/lib/js/main.js
@@ -0,0 +1,14 @@
+﻿<reference path="lib/knockout/knockout-latest.js" />
+require.config({
+    baseUrl: "js",
+    paths: {
+        knockout: "lib/knockout/knockout-latest"
+    }
+});
+
+
+require(['knockout', 'viewModel'], function (ko, vm) {
+    //console.log(vm.name);
+    ko.applyBindings(vm);
+    ko.
+});
---
+++ Exercise3_2_StarterProject/wwwroot/css/lib/js/viewModel.js
@@ -0,0 +1,41 @@
+﻿define(['knockout'], function (ko) {
+    //private part
+    let names = ko.observableArray(
+        [{ name: "Peter" }, { name: "Sue" }, { name: "Ellen" }]);
+    let firstName = ko.observable("Peter");
+    let lastName = ko.observable("Smith");
+    let fullName = ko.computed(function() {
+        return firstName() + " " + lastName();
+    });
+
+    let deleteName = function(data) {
+        names.remove(data);
+    }
+
+    let clickButton = function() {
+        names.push({ name: fullName() });
+        firstName("");
+        lastName("");
+
+    }
+
+
+    fetch("api/categories")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            names(data);
+        });
+
+
+    //public part
+    return {
+        firstName,
+        lastName,
+        fullName,
+        clickButton,
+        names,
+        deleteName
+    };
+});
