f957135c39f4f0580d6c520d8657ecb437d70ed0
Mathias <mip@ruc.dk>
2020-11-19 12:49:47 +0100

    ff

--- Exercise3_2_StarterProject/wwwroot/css/lib/twitter-bootstrap/js/main.js
+++ Exercise3_2_StarterProject/wwwroot/css/lib/twitter-bootstrap/js/main.js
@@ -1,1 +1,13 @@
-﻿
+﻿<reference path="lib/knockout/knockout-latest.js" />
+require.config({
+    baseUrl: "js",
+    paths: {
+        knockout: "lib/knockout/knockout-latest"
+    }
+});
+
+
+require(['knockout', 'viewModel'], function (ko, vm) {
+    //console.log(vm.name);
+    ko.applyBindings(vm);
+});
